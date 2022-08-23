13b0aca9582d6ffbfa0c40b9863cd4100f851522
Jascha Haldemann <jh@oxon.ch>
2022-08-23 18:57:38 +0200

    background.mp4 with random start time

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-// fix for beercss forms with hotwire
 document.addEventListener("turbo:load", function (e) {
+  // fix for beercss forms with hotwire
   if (ui != undefined) {
     ui();
   }
+
+  // background video: set random start time
+  document.getElementById("background-video").currentTime = Math.floor(Math.random()*5); // 5 = max video duration
 })
 
 // Uncomment to disable hotwire
