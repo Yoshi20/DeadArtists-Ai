68c68ca4a24f39a976d9a46d99b6b935c07d1153
Jascha Haldemann <jh@oxon.ch>
2022-08-25 22:47:08 +0200

    nfts: turbo-frame

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,16 +6,10 @@ import "beercss/dist/cdn/beer.min.js"
 import "@hotwired/turbo-rails"
 import "./controllers"
 
+// fix for beercss forms with hotwire
 document.addEventListener("turbo:load", function (e) {
-  // fix for beercss forms with hotwire
   if (ui != undefined) {
     ui();
-  }
-
-  // background video: set random start time
-  let vid = document.getElementById("background-video")
-  if (vid != undefined) {
-    vid.currentTime = Math.floor(Math.random()*5); // 5 = max video duration
   }
 })
 
