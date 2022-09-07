90dda7c268bcd1d1e4c65562d35a241f56c8ce63
Jascha Haldemann <jh@oxon.ch>
2022-09-07 21:26:57 +0200

    all .gif -> .mp4 and other performance improvements

--- app/javascript/controllers/welcome_controller.js
+++ app/javascript/controllers/welcome_controller.js
@@ -6,12 +6,29 @@ export default class extends Controller {
 
   }
 
+  // Triggers with -> data: { action: "click->welcome#enter" }
   enter(e) {
     e.preventDefault();
     document.getElementById('primary').style.display= '';
     document.getElementById('overlay').style.display= 'none';
     document.getElementById('home-title-video').currentTime = 0;
     window.scrollTo(0, 0);
+  }
+
+  // Triggers with -> data: { action: "click->welcome#start_picasso_frame" }
+  start_picasso_frame() {
+    document.getElementById('visible-picasso-frame').style.display = 'none';
+    let hidden_picasso_frame = document.getElementById('hidden-picasso-frame');
+    hidden_picasso_frame.style.display = 'inline-block';
+    hidden_picasso_frame.getElementsByTagName('video')[0].currentTime = 0;
+    hidden_picasso_frame.getElementsByTagName('video')[0].play();
+    setTimeout(() => {document.getElementById('enter-arrows').style.display = '';}, 5000);
+  }
+
+  // Triggers with -> data: { action: "click->welcome#stop_picasso_frame" }
+  stop_picasso_frame() {
+    document.getElementById('hidden-picasso-frame').style.display = 'none';
+    document.getElementById('visible-picasso-frame').style.display = 'inline-block';
   }
 
 }
