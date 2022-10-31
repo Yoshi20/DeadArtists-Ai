7f413b8437014a69e4e6027c284d6ea7f983067e
Jascha Haldemann <jh@oxon.ch>
2022-10-31 12:21:55 +0100

    wl minting end date countdown

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -1,46 +1,46 @@
 import { Controller } from "@hotwired/stimulus"
 
-let countdownInterval = 0;
-
 // Connects to -> data: { controller: 'countdown' }
 export default class extends Controller {
 
   disconnect() {
-    clearInterval(countdownInterval);
-    countdownInterval = 0;
+    clearInterval(this.countdownInterval);
+    this.countdownInterval = 0;
   }
 
   connect() {
-    const publicMintingStartDate = document.getElementById('public-minting-start-date').innerHTML;
-    console.log(publicMintingStartDate);//blup
-    const endDate = new Date().getTime() + 32000;//blup: only for testing
+    const t = this;
+    const endDate = t.element.querySelector('#countdown-end-date').innerHTML;
+    console.log(endDate);//blup
+    const endDateTest = new Date().getTime() + 12000;//blup: only for testing
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-    //blup const countDown = new Date(publicMintingStartDate).getTime();
-    const countDown = new Date(endDate).getTime();
-    countdownInterval = setInterval(function() {
+    // const countDown = new Date(endDate).getTime(); //blup
+    const countDown = new Date(endDateTest).getTime();
+    t.countdownInterval = setInterval(function() {
       const now = new Date().getTime();
       const distance = countDown - now;
-      document.getElementById("days").innerText = Math.floor(distance / (day));
-      document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
-      document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
-      document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-      //do something later when date is reached
       if (distance < 0) {
-        const headline = document.getElementById("headline");
-        headline.innerText = "Public Minting started!";
+        // do something when end date is reached
+        const headline = t.element.querySelector('#headline');
+        headline.innerText = t.element.querySelector('#new-headline').innerText;
         let a = document.createElement("a");
         a.classList.add('color-default');
         a.classList.add('refresh-link');
         a.innerHTML = '-> Refresh page <-';
         a.href = '/mint'
         headline.parentElement.appendChild(a);
-        document.getElementById("countdown").style.display = "none";
-        clearInterval(countdownInterval);
+        // t.element.querySelector('#countdown').style.display = "none";
+        clearInterval(t.countdownInterval);
+        t.countdownInterval = 0;
+      } else {
+        t.element.querySelector('#days').innerText = Math.floor(distance / (day));
+        t.element.querySelector('#hours').innerText = Math.floor((distance % (day)) / (hour));
+        t.element.querySelector('#minutes').innerText = Math.floor((distance % (hour)) / (minute));
+        t.element.querySelector('#seconds').innerText = Math.floor((distance % (minute)) / second);
       }
-      //seconds
-    }, 0)
+    }, 1000)
   }
 }
