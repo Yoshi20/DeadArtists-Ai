c20116cc2c0a0f747031ffb11e814d26cf4875eb
Jascha Haldemann <jh@oxon.ch>
2022-10-31 20:48:47 +0100

    @demo_nft & traits partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,11 @@ class HomeController < ApplicationController
 
   # GET /home
   def index
-
+    count = Rails.cache.fetch("nft_count", expires_in: 24.hours) do
+      Nft.count
+    end
+    @demo_nft = Nft.offset(rand(count)).first
+    @demo_nft.gif_link_no_id = @demo_nft.image_link unless @demo_nft.gif_link_no_id.present? #blup
   end
 
 end
